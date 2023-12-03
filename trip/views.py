df2fe0018a0e824810947a108d794d6cbe25abfe
samzzii <nasstromsamuel@icloud.com>
2023-12-03 21:14:18 +0100

    added a couple of trips in the admin and displayed them in the view

--- trip/views.py
+++ trip/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import TemplateView
 
+from .models import Trip
+
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'trip/index.html'
+    
+
+
+def trips_list(request):
+    trips = Trip.objects.all()
+    context = {
+        'trips': trips,
+    }
+    
+    return render(request, 'trip/trips_list.html', context)

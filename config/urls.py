610353e8985baae032615ca34a59d9d18fa93e5a
samzzii <nasstromsamuel@icloud.com>
2023-12-04 11:11:41 +0100

    added urls for authentication

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,17 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trip.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done]
+# accounts/password_reset/ [name='password_reste']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+#accounts/reset/done [name='password_reset_complete']
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
